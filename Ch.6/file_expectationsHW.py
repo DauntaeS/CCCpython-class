6d7f4a09d6f5e7061bf0b542848efcf746717057
Dauntae <dauntae9@gmail.com>
2024-06-19 13:05:38 -0500

    comment added

---
+++ Ch.6/file_expectationsHW.py
@@ -0,0 +1,15 @@
+# Write a program that asks the user for his or her name, then asks the user to enter a sentence that describes himself or herself. Here is an example of the program’s screen: Submit a screenshot of output that should include a screenshot of the folder where your html file is created and a screenshot of the file opened in the browser.
+
+# Enter your name: John Doe [Enter]
+# Describe yourself: I am a computer science major
+# Once the user has entered the requested input, the program should create an HTML file, containing the input, for a simple Web page. Here is an example of the HTML content, using the sample input previously shown:
+
+# <html>
+# <body>
+# <h1>John Doe</h1>
+# <hr />
+# I am a computer science major
+# <hr />
+# </body>
+# </html>
+# This is how your html file should look when opened in a browser.
