b2ab79717165ec6d7b0c7d5dee6e204cf93e4ea3
Dauntae <dauntae9@gmail.com>
2024-07-06 14:32:16 -0500

    Finished

--- Ch.8 Dictionaries & Sets/Dictionaries&SetsHW.py
+++ Ch.8 Dictionaries & Sets/Dictionaries&SetsHW.py
@@ -3,6 +3,20 @@ from tabulate import tabulate
 # Read the attached file which contains product, amount and price separated by commas in a text file.
 # Summarize the data so that it looks as follows. The first part of the output is a dictionary and the second part is formatted output from the dictionary. Total sales is amount*price.
 
+# Input
+# file meant to be read
+# empty dictionary
+
+# Process
+# open/close sales_data.txt file
+# Iterate through each line of of the file
+# Seperate input data by commas
+# Remove blank spacing
+# add items to dictionary
+
+# Output
+# Table containing data from dictionary
+
 
 def main():
     # opens the file for reading
