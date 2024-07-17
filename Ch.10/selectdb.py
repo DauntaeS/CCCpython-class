fe8290adcaa9829235bf57994427ac3eea1570eb
Dauntae <dauntae9@gmail.com>
2024-07-17 18:40:39 -0500

    Adjustments

--- Ch.10/selectdb.py
+++ Ch.10/selectdb.py
@@ -5,15 +5,24 @@ def main():
     conn = sqlite3.connect("customers.db")
     cur = conn.cursor()
 
-    user_request = int(input("Enter the minimum sales: "))
+    cur.execute("""SELECT * FROM Customer""")
+    all_results = cur.fetchall()
+    print("All customers\n", all_results)
+    print()
 
-    # cur.execute("""SELECT * FROM Customer""")
-    # cur.execute("""SELECT * FROM Customer WHERE Sales_amount > 1000""")
-    cur.execute(f"""SELECT * FROM Customer WHERE Sales_amount > {user_request}""")
+    cur.execute("""SELECT * FROM Customer WHERE Sales_amount > 1000""")
+    over1k = cur.fetchall()
 
-    results = cur.fetchall()
+    print("Customers with sales greater than 1000\n")
+    for val in over1k:
+        print(f"{val[0]:5} {val[2]:5}")
+    print()
 
-    for row in results:
+    user = int(input("Enter the minimum sales: "))
+    cur.execute(f"""SELECT * FROM Customer WHERE Sales_amount > {user}""")
+    user_request = cur.fetchall()
+
+    for row in user_request:
         print(f"{row[0]:5} {row[2]:5}")
 
     conn.commit()
